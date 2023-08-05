86eaad9d14d0cc000e3c79abc396e2abaada34b5
sachin jadhao <jadhaosachin7171@gmail.com>
2023-08-05 11:27:55 +0530

    should work now

--- src/main/java/com/driver/services/TrainService.java
+++ src/main/java/com/driver/services/TrainService.java
@@ -95,18 +95,6 @@ public class TrainService {
             throw new Exception("Invalid stations");
         }
 
-        //Calculate the total seats available
-        //Suppose the route is A B C D
-        //And there are 2 seats available in total in the train
-        //and 2 tickets are booked from A to C and B to D.
-        //The seat is available only between A to C and A to B. If a seat is empty between 2 station
-        // it will be counted to our final ans
-        //even if that seat is booked post the destStation or before the boardingStation
-        //In-short : a train has totalNo of seats and there are tickets from and to different locations
-        //We need to find out the available seats between the given 2 stations.
-
-
-
         return null;
     }
 
